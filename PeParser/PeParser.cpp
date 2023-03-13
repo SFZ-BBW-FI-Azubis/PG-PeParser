68af089103f0d9b1abb4aa46c25ca05c3720f113
Paul Gigel <p.gigel@bbw-fi.de>
2023-03-13 07:19:28 +0100

    Projektdateien hinzufügen.

---
+++ PeParser/PeParser.cpp
@@ -0,0 +1,26 @@
+﻿// PeParser.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+#pragma once
+#include "PeParser.h"
+#include <Windows.h>
+#include <type_traits>
+#include <concepts>
+#include "PeParser.h"
+using namespace std;
+int main()
+{
+	wchar_t fileName[UINT16_MAX] = L"C:/NeuerOrdner(2)/depends.exe";
+	PEParserNamespace::PEParser peparser(fileName);
+	std::cout<< peparser.hFile<<"\n";
+	/*char fileNamee[UINT16_MAX] = "C:/NeuerOrdner(2)/depends.exe";
+	cout <<
+		PEParserNamespace::openFile<const char*>(fileNamee, &peparser).hFile
+		<<endl;
+	
+	wchar_t fileName[UINT16_MAX] = L"C:/NeuerOrdner(2)/depends.exe";
+	cout <<
+		PEParserNamespace::openFile<const wchar_t*>(fileName, &peparser).hFile
+		<< endl;
+	*/
+	return 0;
+}
---
+++ PeParser/PeParser.h
@@ -0,0 +1,61 @@
+﻿// PeParser.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <iostream>
+#include <Windows.h>
+namespace PEParserNamespace {
+	//concepts
+	template<typename T> concept is_char = std::same_as<T, const char*>;
+	template<typename T> concept is_wchar_t = std::same_as<T, const wchar_t*>;
+	template<class T> concept impl_PEParserBase = requires(T type) {
+		{type.hFile};
+	};
+	template<typename T> concept has_this = requires(T type) {
+		true;
+	};
+	//openFile declaration
+	template<typename T = const wchar_t*, class PEParserBaseImpl = PEParser>
+		requires (is_char<T> || is_wchar_t<T>) && impl_PEParserBase<PEParserBaseImpl>
+	inline PEParserBaseImpl & openFile(T lpFileName, PEParserBaseImpl * pPEParserBaseImpl) noexcept;
+
+	class PEParserBase {
+	public:
+		HANDLE hFile;
+		DWORD dwFileSize;
+	};
+	//friends/hosts methods
+	class PEParser : public PEParserBase{
+	public:
+		PEParser(const wchar_t* lpFileName) {
+			openFile<const wchar_t*, PEParser>(lpFileName, this);
+		}
+		PEParser() {
+		};
+		template<typename T, class PEParserBaseImpl>
+		friend inline PEParser& openFile<T, PEParserBaseImpl>(T, PEParserBaseImpl*) noexcept;
+	};
+	//openFile implementation
+	//"thin" wrapper for CreateFile
+	template<typename T, class PEParserBaseImpl>
+	requires (is_char<T> || is_wchar_t<T>) && impl_PEParserBase<PEParserBaseImpl>
+	inline PEParserBaseImpl& openFile(T lpFileName , PEParserBaseImpl * pPEParserBaseImpl) noexcept {
+		if constexpr (is_char<T>) {
+			pPEParserBaseImpl->hFile = CreateFileA(lpFileName, GENERIC_READ | GENERIC_WRITE,
+				0, nullptr, OPEN_EXISTING,
+				FILE_ATTRIBUTE_NORMAL, NULL);
+		}
+		else {
+			pPEParserBaseImpl->hFile = CreateFileW(lpFileName, GENERIC_READ | GENERIC_WRITE,
+				0, nullptr, OPEN_EXISTING,
+				FILE_ATTRIBUTE_NORMAL, NULL);
+			std::wcout << std::endl << lpFileName << std::endl;
+		}
+		return *pPEParserBaseImpl;
+	};
+	void test() {
+		PEParser pepeDeFrog;
+		
+	};
+}
