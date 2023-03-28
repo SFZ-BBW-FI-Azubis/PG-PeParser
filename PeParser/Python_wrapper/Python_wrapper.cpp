cb6b01b8329e5af03354381c9a922776282e9616
Paul Gigel <p.gigel@bbw-fi.de>
2023-03-28 10:17:41 +0200

    dll compiles

---
+++ PeParser/Python_wrapper/Python_wrapper.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include <windows.h>
+#include <stdio.h>
+//#include <Python.h>
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    switch (fdwReason)  {
+        case DLL_PROCESS_ATTACH:    {
+            printf("Initialize once for each new process...\n");
+            break;
+        }
+        case DLL_THREAD_ATTACH: {
+            printf("Do thread-specific initialization...\n");
+            break;
+        }
+        case DLL_THREAD_DETACH:  {
+            printf("Do thread-specific cleanup...\n");
+            break;
+        }
+        case DLL_PROCESS_DETACH:    {
+            printf("Perform any necessary cleanup...\n");
+            break;
+        }
+    }
+    return TRUE;
+}
