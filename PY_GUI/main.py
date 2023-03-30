52ff50040331e4060aaba96cf7e5e7c27bdd8bd6
Paul Gigel <p.gigel@bbw-fi.de>
2023-03-30 08:05:38 +0200

    almost perfect
    print(testWrapper(pEParserBase, _test).bytes)
    print(pEParserBase.bytes)
    #does not get the same output
    #but it should

--- PY_GUI/main.py
+++ PY_GUI/main.py
@@ -1,5 +1,6 @@
 #Paul stinkt
 import ctypes
+#DataStructures.py
 class PEParserBase(ctypes.Structure):
     _fields_ = [("hFile", ctypes.c_void_p),
         ("dwFileSize", ctypes.c_ulong),
@@ -7,16 +8,17 @@ class PEParserBase(ctypes.Structure):
         ("fileBuffer",ctypes.c_void_p)]
 class PEHEADER(ctypes.Structure):
     xc = 0
-def testWrapper(peParserBase):
-    print("Hallo welt")
+#FunctionWrapper.py
+def testWrapper(peParserBase, _instanceDLL):
+    _instanceDLL.test.argtypes = [ctypes.POINTER(PEParserBase)]
+    _instanceDLL.test.restype = PEParserBase
+    return _instanceDLL.test(ctypes.byref(pEParserBase))
+
 pEParserBase = PEParserBase()
 print("hallo welt")
 _test = ctypes.CDLL('PeParserPythonWrapper.dll')
-_test.test.argtypes = [ctypes.POINTER(PEParserBase)]
-_test.test.restype = PEParserBase
-result = _test.test(ctypes.byref(pEParserBase))
-print(result.bytes)
-x = ctypes.c_int.in_dll(_test, "x")
-print(x);
-testWrapper
-#print(int(_test.test(None)))
+
+#x = ctypes.c_int.in_dll(_test, "x")
+#print(x);
+print(testWrapper(pEParserBase, _test).bytes)
+print(pEParserBase.bytes)
