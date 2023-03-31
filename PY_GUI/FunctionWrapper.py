4683dd9a2954fdcd66d6d2d17512cbb16e97ba17
Paul Gigel <p.gigel@bbw-fi.de>
2023-03-31 14:33:21 +0200

    cleanup

--- PY_GUI/FunctionWrapper.py
+++ PY_GUI/FunctionWrapper.py
@@ -1,12 +1,12 @@
 import DataStructures
 import ctypes
 class FunctionWraper:
-    @classmethod
+    #@classmethod
     def testWrapper(self, peParserBase, _instanceDLL):
         _instanceDLL.test.argtypes = [ctypes.POINTER(DataStructures.PEParserBase)]
         _instanceDLL.test.restype = ctypes.POINTER(DataStructures.PEParserBase)
         return _instanceDLL.test(ctypes.byref(peParserBase))
-    @classmethod
+    #@classmethod
     def test2Wrapper(self, peHEADER, _instanceDLL):
         _instanceDLL.test2.argtypes = [ctypes.POINTER(DataStructures.PEHEADER)]
         _instanceDLL.test2.restype = ctypes.POINTER(DataStructures.PEHEADER)
--- PY_GUI/main.py
+++ PY_GUI/main.py
@@ -1,25 +1,6 @@
-#Paul stinkt
 import ctypes
 import DataStructures
-#from "PY_GUI\FunctionWrapper.py" 
 import FunctionWrapper
-#DataStructures.py
-#class PEParserBase(ctypes.Structure):
-#    _fields_ = [("hFile", ctypes.c_void_p),
-#        ("dwFileSize", ctypes.c_ulong),
-#        ("bytes", ctypes.c_ulong),
-#        ("fileBuffer",ctypes.c_void_p)]
-#class PEHEADER(ctypes.Structure):
-#    _fields_ = [("test", PEParserBase)]
-#FunctionWrapper.py
-#def testWrapper(peParserBase, _instanceDLL):
-#    _instanceDLL.test.argtypes = [ctypes.POINTER(PEParserBase)]
-#    _instanceDLL.test.restype = ctypes.POINTER(PEParserBase)
-#    return _instanceDLL.test(ctypes.byref(pEParserBase))
-#def test2Wrapper(pEHEADER, _instanceDLL):
-#    _instanceDLL.test2.argtypes = [ctypes.POINTER(PEHEADER)]
-#    _instanceDLL.test2.restype = ctypes.POINTER(PEHEADER)
-#    return _instanceDLL.test2(ctypes.byref(pEHEADER))
 
 pEParserBase = DataStructures.PEParserBase()
 pEHEADER = DataStructures.PEHEADER()
