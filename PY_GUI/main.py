843ab4fce539e274ecc6c994e252e8906f910814
Paul Gigel <p.gigel@bbw-fi.de>
2023-03-31 09:59:32 +0200

    POINTER

--- PY_GUI/main.py
+++ PY_GUI/main.py
@@ -11,15 +11,14 @@ class PEHEADER(ctypes.Structure):
 #FunctionWrapper.py
 def testWrapper(peParserBase, _instanceDLL):
     _instanceDLL.test.argtypes = [ctypes.POINTER(PEParserBase)]
-    _instanceDLL.test.restype = PEParserBase
+    _instanceDLL.test.restype = ctypes.POINTER(PEParserBase)
     return _instanceDLL.test(ctypes.byref(pEParserBase))
 
 pEParserBase = PEParserBase()
 print("hallo welt")
 _test = ctypes.CDLL('PeParserPythonWrapper.dll')
-
-#x = ctypes.c_int.in_dll(_test, "x")
-#print(x);
-#id should be the same, but it is not
-print(id(testWrapper(pEParserBase, _test)))
-print(id(pEParserBase))
+pEParserBase = PEParserBase()
+pPEParserBase = testWrapper(pEParserBase, _test)
+print(testWrapper(pEParserBase, _test).contents.bytes)
+print(hex(ctypes.addressof(pEParserBase)), pEParserBase.bytes)
+print(hex(ctypes.addressof(pPEParserBase.contents)), pEParserBase.bytes)
