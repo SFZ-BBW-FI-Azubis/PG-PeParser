953ddfbd9ea826515d9a1e5721423204a17446fc
Paul Gigel <p.gigel@bbw-fi.de>
2023-04-11 13:30:28 +0200

    qewrqwer

--- PeParser/Python_wrapper/Dummy.cpp
+++ PeParser/Python_wrapper/Dummy.cpp
@@ -1,7 +1,7 @@
 //Dummy.cpp will be responsable for parsing Funktion names (because of namemangeling)
 //Dummy.cpp will be responsable for writing .def Files
 //Dummy.cpp will map Function name to ordinal SYSTEMATICALLY
-#include "../Declarations.h";
+#include "../Declarations.h"
 int main() {
 	return 0;
 }
