d1a2d02d8e2dc32ed6395c3369dc3b4e4a8c897d
Paul Gigel <p.gigel@bbw-fi.de>
2023-04-17 14:32:27 +0200

    its working
    had a small type, that took me hours to find

--- PeParser/PeParser.cpp
+++ PeParser/PeParser.cpp
@@ -4,12 +4,55 @@
 #define _call // use standart calling convention, that is __thiscall
 #include "Declarations.h"
 #include <Windows.h>
+
+template <class InIt, class Fn>
+void for_each(InIt _First, InIt _Last, Fn _Func) {
+	auto _UFirst = _First;
+	auto _ULast = _Last;
+	for (; _UFirst <= _ULast; ++_UFirst) {
+		_Func(*_UFirst);
+	}
+
+	return;
+}
+
+template<typename T>
+class Iterable {
+	typedef void (__cdecl * Callback)(int&);
+	T begin;
+	T end;
+	Callback callback;
+public:
+	const Iterable(T t, size_t size) noexcept :
+		end(&t[size -1]),
+		begin(&t[0]) {};
+	const Iterable(T t, size_t size, Callback callback) noexcept :
+		begin(&t[0]),
+		end(&t[size -1]), 
+		callback(callback) {};
+	const auto operator()(Callback callback) noexcept {
+		return for_each(begin, end, callback);
+	}
+	const auto operator()() noexcept {
+		return for_each(begin, end, callback);
+	}
+};
 using namespace std;
-int main()
-{
+int main()	{
 	typedef PEParserNamespace::PEParser PEParser;
 	PEParser peparser;
-
+	
+	int myvector[5] = {0,1,2,3,4};
+	Iterable iterable(myvector, 5);
+	iterable([](int &single)->void {
+		std::cout << single << std::endl;
+		});
+	int myvector2[5] = { 5,6,7,8,9 };
+	Iterable iterable2(myvector2, 5, [](int& single)->void {
+		std::cout << single << std::endl;
+		});
+	iterable2();	// I called iterable() -> that small mistake cost me a whole day to find :(
+	
 	char fileNamee[UINT16_MAX] = "C:/NeuerOrdner(2)/depends.exe";
 	cout <<
 		PEParserNamespace::openFile<const char*>(fileNamee, &peparser).hFile
